d80ecb926fccf44b4c7e8909be00da881ac25a5d
bot2333 <asdc199@gmail.com>
2024-02-20 13:23:11 +0800

    Update diy-part1.sh

--- padavanonly/Redmi-AX6000/diy-part1.sh
+++ padavanonly/Redmi-AX6000/diy-part1.sh
@@ -25,14 +25,3 @@ sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.d
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a -f -p small8
-
-
-
-./scripts/feeds uninstall luci-app-mosdns mosdns v2ray-geodata
-./scripts/feeds install -f -p mosdns mosdns luci-app-mosdns
-find ./ -name v2ray-geodata | xargs rm -rf
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-svn export https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-vlmcsd package/luci-app-vlmcsd
-svn export https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/vlmcsd  package/vlmcsd
-sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' ./package/*/Makefile
-./scripts/feeds update -i && ./scripts/feeds install -a
